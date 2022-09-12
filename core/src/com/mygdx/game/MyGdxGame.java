0b1fafd20bf08d79fef594ebb09451ff43770e79
lasteik <lastevilday@gmail.com>
2022-09-12 14:50:26 +0500

    init hw1

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,34 +8,33 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	AnimationOne anim;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("beckground.jpg");
-		anim = new AnimationOne("pngwing.png",3,4,24f, Animation.PlayMode.LOOP);
-	}
+    SpriteBatch batch;
+    Texture img;
+    AnimationOne anim;
 
-	@Override
-	public void render () {
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("beckground.jpg");
+        anim = new AnimationOne("pngwing.png", 3, 4, 24f, Animation.PlayMode.LOOP);
+    }
 
-		anim.setTime(Gdx.graphics.getDeltaTime());
-		float x = Gdx.input.getX()-anim.draw().getRegionHeight()/2;
-		float y = Gdx.graphics.getWidth() - Gdx.input.getY()-anim.draw().getRegionWidth()+10;
-		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
-		batch.draw(img,0,0);
-		batch.draw(anim.draw(),x,y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		anim.dispose();
-		img.dispose();
-	}
+    @Override
+    public void render() {
+        anim.setTime(Gdx.graphics.getDeltaTime());
+        float x = Gdx.input.getX() - anim.draw().getRegionHeight() / 2;
+        float y = Gdx.graphics.getWidth() - Gdx.input.getY() - anim.draw().getRegionWidth() + 10;
+        ScreenUtils.clear(1, 1, 1, 1);
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.draw(anim.draw(), x, y);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        anim.dispose();
+        img.dispose();
+    }
 }
